5bd92c77fee76a32bdea6b0aa4922a40d67ee143
damodotun <damodotunadedeji@gmail.com>
2025-07-17 09:49:42 +0100

    First Commit

---
+++ introduction.js
@@ -0,0 +1,58 @@
+    // Const
+    const PI = 3.14
+    console.log(PI);
+    // PI = 65
+    // console.log(PI);
+
+
+    let Welcome = 'Welcome to Javascript'
+    console.log(Welcome);
+    let thisClass = 'Javascript'
+    console.log(thisClass);
+
+    // let var const
+    let previousClass = 'Html/Css'
+    console.log(previousClass);
+    previousClass = 'Javascript'
+    console.log(previousClass);
+
+    console.log('Welcome To Javascript Class');
+    console.log('Welcome To Javascript Class');
+
+    var nextClass = 'React'
+    console.log(nextClass);
+    nextClass = 'Javascript'
+    console.log(nextClass);
+
+        // CONST
+    // const PI = 3.14
+    // console.log(PI)
+    // // PI = 634373
+    // // console.log(PI);
+
+
+    // let Welcome = 'Welcome To JS'
+    // console.log(Welcome);
+
+    // let var const
+
+    // let thisClass = 'Javascript'
+    // console.log(thisClass);
+    // thisClass = 'Html/Css'
+    // console.log(thisClass);
+
+
+    let divFromDOM = document.getElementById('newDiv')
+    console.log(divFromDOM);
+
+    divFromDOM.innerHTML = 'Hello Everybody'
+    divFromDOM.style.width = '300px'
+    divFromDOM.style.height = '300px'
+    divFromDOM.style.backgroundColor = 'red'
+    divFromDOM.style.display = 'flex'
+
+    let h1Tag = document.getElementById('h1Tag')
+    console.log(h1Tag);
+    h1Tag.innerHTML = 'Welcome Everybody'
+
+
